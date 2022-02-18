8ee4a6f3186e1fad5cb919ed116971583f697e7c
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-02-18 12:59:57 +0700

    Initial commit

---
+++ app/src/main/java/com/refeed_ppb1/androidbroadcast/CustomReceiver.java
@@ -0,0 +1,32 @@
+package com.refeed_ppb1.androidbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String msg = "";
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED:
+                msg = "the power is connected";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                msg = "the power is disconnected";
+                break;
+            case "ACTION_CUSTOM_BROADCAST":
+//                msg = "this is a custom broadcast, data: ";
+                msg = intent.getStringExtra("data");
+                msg.concat(intent.getStringExtra("data"));
+                break;
+            default:
+                break;
+        }
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
